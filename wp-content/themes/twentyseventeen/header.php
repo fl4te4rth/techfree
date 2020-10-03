c01ffb53dbe3d55e7fb5937d6a10d90c65ef18ee
fl4te4rth <46136620+fl4te4rth@users.noreply.github.com>
2020-10-03 10:14:04 +0300

    Update header.php

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -15,6 +15,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
+<meta name="clckd" content="1483255f33abcef90e5126a8ea660d8f" />
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
